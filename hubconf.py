fdba82bfc9d0dfaf5eaf7648279ee0b7fad86e8c
cs19b011iittp <69444649+cs19b011iittp@users.noreply.github.com>
2022-10-19 11:00:12 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -10,11 +10,19 @@ class cs19b011NN(nn.Module):
     def __init__(self, in_channels, w, h, num_classes):
         super().__init__()
         self.conv1 = nn.Conv2d(
-            in_channels=in_channels, out_channels=10, kernel_size=(2, 2), padding='same')
+            in_channels=in_channels, 
+            out_channels=10, 
+            kernel_size=(2, 2), 
+            padding='same')
         self.relu1 = nn.ReLU()
+        
         self.conv2 = nn.Conv2d(
-            in_channels=10, out_channels=20, kernel_size=(2, 2), padding='same')
+            in_channels=10, 
+            out_channels=20, 
+            kernel_size=(2, 2), 
+            padding='same')
         self.relu2 = nn.ReLU()
+        
         # self.pool1 = nn.MaxPool2d(kernel_size=(2,2),stride=(2,2))
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(in_features=w*h*20, out_features=num_classes)
@@ -44,29 +52,41 @@ def get_model(train_data_loader=None, n_epochs=10):
 
 
 def get_model_advanced(train_data_loader=None, n_epochs=10, lr=1e-4, config=None):
-    model = None
+  iter = 0
+  
 
-    # write your code here as per instructions
-    # ... your code ...
-    # ... your code ...
-    # ... and so on ...
-    # Use softmax and cross entropy loss functions
-    # set model variable to proper object, make use of train_data
+  for epoch in range(n_epochs):
 
-    # In addition,
-    # Refer to config dict, where learning rate is given,
-    # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
-    # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
-    # You need to create 2d convoution layers as per specification above in each element
-    # You need to add a proper fully connected layer as the last layer
+    model = get_model(config["in_channels"],config["out_channels"],config["kernel_size"],config["stride"],config["padding"])
+    criterion = nn.CrossEntropyLoss()
+    learning_rate = lr
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-    # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
-    # HINT: Flatten function can also be used if required
-    return model
+    for i, (images, labels) in enumerate(train_data_loader):
+        # Load images as tensors with gradient accumulation abilities
+        images = images.requires_grad_()
 
-    print('Returning model... (rollnumber: xx)')
+        # Clear gradients w.r.t. parameters
+        optimizer.zero_grad()
 
-    return model
+        # Forward pass to get output/logits
+        outputs = model(images)
+
+        # Calculate Loss: softmax --> cross entropy loss
+        loss = criterion(outputs, labels)
+
+        # Getting gradients w.r.t. parameters
+        loss.backward()
+
+        # Updating parameters
+        optimizer.step()
+
+  
+  iter+=1
+
+  print ('Returning model... (rollnumber: 11)')
+  
+  return model
 
 # sample invocation torch.hub.load(myrepo,'test_model',model1=model,test_data_loader=test_data_loader,force_reload=True)
 
