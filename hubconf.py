432795f495aaf2a49c3dbe8f5c0e3d2c5ef67ae7
cs19b011iittp <69444649+cs19b011iittp@users.noreply.github.com>
2022-10-19 11:06:36 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -5,38 +5,32 @@ from torch import nn
 
 
 class cs19b011NN(nn.Module):
-    # ... your code ...
-    # ... write init and forward functions appropriately ...
-    def __init__(self, in_channels, w, h, num_classes):
-        super().__init__()
-        self.conv1 = nn.Conv2d(
-            in_channels=in_channels, 
-            out_channels=10, 
-            kernel_size=(2, 2), 
-            padding='same')
+    def __init__(cs19b011NN, self, input, output, kernel, strid, pad):
+
+        # Convolution 1
+        self.cnn1 = nn.Conv2d(in_channels=self.input, out_channels=self.output, kernel_size=self.kernel, stride=self.strid, padding=self.pad)
         self.relu1 = nn.ReLU()
-        
-        self.conv2 = nn.Conv2d(
-            in_channels=10, 
-            out_channels=20, 
-            kernel_size=(2, 2), 
-            padding='same')
-        self.relu2 = nn.ReLU()
-        
-        # self.pool1 = nn.MaxPool2d(kernel_size=(2,2),stride=(2,2))
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(in_features=w*h*20, out_features=num_classes)
-        self.softmax = nn.LogSoftmax()
+
+        # Max pool 1
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+
+        # Fully connected 1 (readout)
+        self.fc1 = nn.Linear(32 * 7 * 7, 10) 
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.softmax(x)
-        return x
+        # Convolution 1
+        out = self.cnn1(x)
+        out = self.relu1(out)
+
+        # Max pool 1
+        out = self.maxpool1(out)
+
+        out = out.view(out.size(0), -1)
+
+        # Linear function (readout)
+        out = self.fc1(out)
+
+        return out
 
 # sample invocation torch.hub.load(myrepo,'get_model',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 
