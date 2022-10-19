be8ef51d128ff9fb6e0ecec7bbef56d5a2db90aa
cs19b011iittp <69444649+cs19b011iittp@users.noreply.github.com>
2022-10-19 11:50:34 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -67,12 +67,11 @@ class cs19b011NN(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
-        # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
         x = x.view(x.size(0), -1)       
         output = self.out(x)
         return output, x
     
-def get_model():
+def get_model(train_data_loader=None, n_epochs=10):
     model = cs19b011NN()
     return model
 
