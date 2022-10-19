763190750310edf08024059fb554e9c5bb952748
cs19b011iittp <69444649+cs19b011iittp@users.noreply.github.com>
2022-10-19 11:24:54 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -6,7 +6,7 @@ from torch import nn
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-class cs19b011NN(nn.Module):
+class cs19b037NN(nn.Module):
     # ... your code ...
     # ... write init and forward functions appropriately ...
     def __init__(self, in_channels, w, h, num_classes):
@@ -28,23 +28,28 @@ class cs19b011NN(nn.Module):
         x = self.conv2(x)
         x = self.relu2(x)
         x = self.flatten(x)
-        print(x.shape)
         x = self.fc1(x)
         x = self.softmax(x)
         return x
 
-class cs19b011AdvancedNN(nn.Module):
-    def __init__(self,modules_list,num_classes):
+
+class cs19b037AdvancedNN(nn.Module):
+    def __init__(self, modules_list, num_classes):
         super().__init__()
+        self.num_classes = num_classes
         self.linears = nn.ModuleList(modules_list)
         self.flatten = nn.Flatten()
         # self.fc1 = nn.Linear(in_features=w*h*20, out_features=num_classes)
+        self.softmax = nn.LogSoftmax()
 
     def forward(self, x):
         for i in range(len(self.linears)):
-            x = self.layers[i](x)
-        x=self.flatten(x)
-        print(x.shape)
+            x = self.linears[i](x)
+        x = self.flatten(x)
+        x = nn.Linear(in_features=x.shape[-1],
+                      out_features=self.num_classes)(x)
+        x = self.softmax(x)
+        return x
 
 # sample invocation torch.hub.load(myrepo,'get_model',train_data_loader=train_data_loader,n_epochs=5, force_reload=True)
 
@@ -75,7 +80,7 @@ def get_model(train_data_loader=None, n_epochs=10):
         num_classes = torch.max(y).item()-torch.min(y).item()+1
         break
 
-    model1 = cs19b011NN(num_channels, w, h, num_classes).to(device)
+    model1 = cs19b037NN(num_channels, w, h, num_classes).to(device)
 
     optimizer = torch.optim.SGD(model1.parameters(), lr=1e-3)
     loss_fn = nn.CrossEntropyLoss()
@@ -99,7 +104,7 @@ def get_model(train_data_loader=None, n_epochs=10):
         print("Epoch", epoch, "accuracy:",
               correct/len(train_data_loader.dataset))
 
-    print('Returning model... (rollnumber: cs19b011)')
+    print('Returning model... (rollnumber: cs19b037)')
 
     return model1
 
@@ -108,17 +113,63 @@ def get_model(train_data_loader=None, n_epochs=10):
 
 def get_model_advanced(train_data_loader=None, n_epochs=10, lr=1e-4, config=None):
 
-    modules_list=[]
+    # write your code here as per instructions
+    # ... your code ...
+    # ... your code ...
+    # ... and so on ...
+    # Use softmax and cross entropy loss functions
+    # set model variable to proper object, make use of train_data
+
+    # In addition,
+    # Refer to config dict, where learning rate is given,
+    # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
+    # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
+    # You need to create 2d convoution layers as per specification above in each element
+    # You need to add a proper fully connected layer as the last layer
+
+    # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
+    # HINT: Flatten function can also be used if required
+
+    num_classes = 0
+    for (X, y) in train_data_loader:
+        num_classes = torch.max(y).item()-torch.min(y).item()+1
+        break
+
+    modules_list = []
     if(config is not None):
         for module in config:
-            modules_list.append(nn.Conv2d(in_channels=module[0], out_channels=module[1],kernel_size=module[2], stride=module[3], padding=module[4]))
+            modules_list.append(nn.Conv2d(
+                in_channels=module[0], out_channels=module[1], kernel_size=module[2], stride=module[3], padding=module[4]))
 
-    model1=cs19b011AdvancedNN(modules_list=modules_list,).to(device)
+    modules_list=modules_list.to(device)
+    model1 = cs19b037AdvancedNN(
+        modules_list=modules_list, num_classes=num_classes).to(device)
+
+    optimizer = torch.optim.SGD(model1.parameters(), lr=1e-3)
+    loss_fn = nn.CrossEntropyLoss()
+
+    # training
+    for epoch in range(n_epochs):
+        model1.train()
+        train_loss = 0
+        correct = 0
+        for batch, (X, y) in enumerate(train_data_loader):
+            X, y = X.to(device), y.to(device)
+            ypred = model1(X)
+            loss = loss_fn(ypred, y)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss
+            correct += (ypred.argmax(1) == y).type(torch.float).sum().item()
+        print("Epoch", epoch, "accuracy:",
+              correct/len(train_data_loader.dataset))
+
+    print('Returning model... (rollnumber: cs19b037)')
+
     return model1
-
-    print('Returning model... (rollnumber: cs19b011)')
-
-    return model
 
 # sample invocation torch.hub.load(myrepo,'test_model',model1=model,test_data_loader=test_data_loader,force_reload=True)
 
@@ -131,6 +182,8 @@ def test_model(model1=None, test_data_loader=None):
     # ... your code ...
     # ... and so on ...
     # calculate accuracy, precision, recall and f1score
+
+
 
     print('Returning metrics... (rollnumber: xx)')
 
