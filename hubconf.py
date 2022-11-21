db65cd3c1afc31ddecff623295fd0574edb73755
cs19b011iittp <69444649+cs19b011iittp@users.noreply.github.com>
2022-11-21 09:38:55 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -22,3 +22,29 @@ def get_data_blobs(n_points=100):
                        cluster_std=0.60, random_state=0)
   plt.scatter(X[:, 0], X[:, 1], s=50);
   return X,y
+
+
+def get_data_circles(n_points=100):
+  pass
+  # write your code here
+  # Refer to sklearn data sets
+  import sklearn.cluster as skl_cluster
+  import sklearn.datasets as skl_data
+
+  circles, circles_clusters = skl_data.make_circles(n_samples=400, noise=.01, random_state=0)
+
+  # cluster with kmeans
+  Kmean = skl_cluster.KMeans(n_clusters=2)
+  Kmean.fit(circles)
+  clusters = Kmean.predict(circles)
+
+  # plot the data, colouring it by cluster
+  plt.scatter(circles[:, 0], circles[:, 1], s=15, linewidth=0.1, c=clusters,cmap='flag')
+  plt.show()
+
+  # cluster with spectral clustering
+  model = skl_cluster.SpectralClustering(n_clusters=2, affinity='nearest_neighbors', assign_labels='kmeans')
+  labels = model.fit_predict(circles)
+  plt.scatter(circles[:, 0], circles[:, 1], s=15, linewidth=0, c=labels, cmap='flag')
+  plt.show()
+  return circles, circles_clusters
